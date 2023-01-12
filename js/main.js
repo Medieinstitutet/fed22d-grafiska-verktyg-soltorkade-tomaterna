c99126d347ab2bda4a5d903766b01ca8bc446c04
YounesEch <114940175+YounesEch@users.noreply.github.com>
2023-01-12 23:49:00 +0100

    påbörjat slideshow section 1 och fixat rutan med lediga jobb

--- js/main.js
+++ js/main.js
@@ -3,11 +3,14 @@ const hamburger = document.querySelector('.menuButton');
 const navMenu = document.querySelector('.openMenu');
 
 const apply = document.querySelector('.applyJob');
+const applyBtn = document.querySelector('.jobs');
+const applyClose = document.querySelector('.closeApply');
 
 hamburger.addEventListener('click', menuOpen);
 navMenu.addEventListener('click', navOpen);
 
-apply.addEventListener('click', openJob);
+applyBtn.addEventListener('click', openJob);
+applyClose.addEventListener('click', closeJob );
 
 function menuOpen () {
     hamburger.classList.remove('active');
@@ -20,5 +23,29 @@ function navOpen () {
 }
 
 function openJob () {
-    apply.classList.toggle('active');
-} //denna vill inte fungera....
+    apply.classList.remove('none');
+}
+function closeJob () {
+    apply.classList.toggle('none');
+}
+
+
+const sideOne = document.querySelector('.background1');
+const sideTwo = document.querySelector('.background2');
+
+
+let nextBtn = document.querySelector('.next');
+let prevBtn = document.querySelector('.prev');
+
+nextBtn.addEventListener('click', nextImage);
+prevBtn.addEventListener('click', prevImage);
+
+function nextImage () {
+    sideOne.classList.toggle('none');
+    sideTwo.classList.remove('none');
+}
+
+function prevImage () {
+    sideOne.classList.remove('none');
+    sideTwo.classList.toggle('none');
+}
